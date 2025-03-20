c5dabf7e48cc8f68269d888e3285fa13cece013d
ngtrhui <trihuy2004@gmail.com>
2025-03-20 20:54:01 +0700

    fixfix

--- models.py
+++ models.py
@@ -3,17 +3,13 @@ from torch import nn
 import torch.nn.functional as F
 from torch.nn.modules.container import ModuleList
 from torch_geometric.nn import (GATv2Conv,
-                                SAGPooling,
-                                LayerNorm,
-                                global_mean_pool,
-                                max_pool_neighbor_x,
-                                global_add_pool)
+                                SAGPooling, 
+                                LayerNorm, 
+                                global_mean_pool, 
+                                global_add_pool
+)
 
-from layers import (
-                    CoAttentionLayer, 
-                    RESCAL, 
-                    RESCAL
-                    )
+from layers import CoAttentionLayer, RESCAL
 
 
 class SSI_DDI(nn.Module):
@@ -21,16 +17,15 @@ class SSI_DDI(nn.Module):
         super().__init__()
         self.in_features = in_features
         self.hidd_dim = hidd_dim
-        self.rel_total = rel_total
         self.kge_dim = kge_dim
-        self.n_blocks = len(blocks_params)
+        self.rel_total = rel_total
         
         self.initial_norm = LayerNorm(self.in_features)
-        self.blocks = []
+        self.blocks = ModuleList()
         self.net_norms = ModuleList()
+        
         for i, (head_out_feats, n_heads) in enumerate(zip(heads_out_feat_params, blocks_params)):
             block = SSI_DDI_Block(n_heads, in_features, head_out_feats, final_out_feats=self.hidd_dim)
-            self.add_module(f"block{i}", block)
             self.blocks.append(block)
             self.net_norms.append(LayerNorm(head_out_feats * n_heads))
             in_features = head_out_feats * n_heads
@@ -40,53 +35,41 @@ class SSI_DDI(nn.Module):
 
     def forward(self, triples):
         h_data, t_data, rels = triples
+        
+        h_data.x = self.initial_norm(h_data.x)
+        t_data.x = self.initial_norm(t_data.x)
 
-        h_data.x = self.initial_norm(h_data.x, h_data.batch)
-        t_data.x = self.initial_norm(t_data.x, t_data.batch)
-
-        repr_h = []
-        repr_t = []
-
+        repr_h, repr_t = [], []
+        
         for i, block in enumerate(self.blocks):
-            out1, out2 = block(h_data), block(t_data)
-
-            h_data = out1[0]
-            t_data = out2[0]
-            r_h = out1[1]
-            r_t = out2[1]
-
+            h_data, r_h = block(h_data)
+            t_data, r_t = block(t_data)
+            
             repr_h.append(r_h)
             repr_t.append(r_t)
-
-            h_data.x = F.gelu(self.net_norms[i](h_data.x, h_data.batch))
-            t_data.x = F.gelu(self.net_norms[i](t_data.x, t_data.batch))
+            
+            h_data.x = F.gelu(self.net_norms[i](h_data.x))
+            t_data.x = F.gelu(self.net_norms[i](t_data.x))
         
         repr_h = torch.stack(repr_h, dim=-2)
         repr_t = torch.stack(repr_t, dim=-2)
 
-        kge_heads = repr_h
-        kge_tails = repr_t
-
-        attentions = self.co_attention(kge_heads, kge_tails)
-        # attentions = None
-        scores = self.KGE(kge_heads, kge_tails, rels, attentions)
-
+        attentions = self.co_attention(repr_h, repr_t)
+        scores = self.KGE(repr_h, repr_t, rels, attentions)
+        
         return scores
 
 
 class SSI_DDI_Block(nn.Module):
     def __init__(self, n_heads, in_features, head_out_feats, final_out_feats):
         super().__init__()
-        self.n_heads = n_heads
-        self.in_features = in_features
-        self.out_features = head_out_feats
         self.conv = GATv2Conv(in_features, head_out_feats, n_heads)
         self.readout = SAGPooling(n_heads * head_out_feats, min_score=-1)
     
     def forward(self, data):
         data.x = self.conv(data.x, data.edge_index)
-        att_x, att_edge_index, att_edge_attr, att_batch, att_perm, att_scores= self.readout(data.x, data.edge_index, batch=data.batch)
-        global_graph_emb = global_add_pool(att_x, att_batch)
-
-        # data = max_pool_neighbor_x(data)
-        return data, global_graph_emb
+        pooled_x, att_edge_index, _, att_batch, _, _ = self.readout(data.x, data.edge_index, batch=data.batch)
+        
+        global_emb = global_add_pool(pooled_x, att_batch) + global_mean_pool(pooled_x, att_batch)
+        
+        return data, global_emb
